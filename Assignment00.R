a109923a6d58471dec63db84353c6439674e7c49
MahshidArastonejad <arastonejadm@vcu.edu>
2023-01-27 18:55:52 -0500

    Change the size

--- Assignment00.R
+++ Assignment00.R
@@ -7,8 +7,8 @@ library(ggplot2)
 ggplot(mtcars, aes(mpg)) +
   geom_histogram(binwidth = 4) + xlab('Miles per Gallon') + ylab('Number of Cars') + 
   ggtitle('Distribution of Cars by Mileage')
-
+#creat scatterplot
 ggplot(mtcars,aes(x = mpg, y = hp))+
   
-  geom_point(color = "red", size = 2.5, shape = 1)+ theme_classic()
+  geom_point(color = "purple", size = 2.5, shape = 1)+ theme_classic()
 
