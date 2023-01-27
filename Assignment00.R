43e4c9553128cfbb6123c894d577988b93da835a
MahshidArastonejad <arastonejadm@vcu.edu>
2023-01-27 18:52:22 -0500

    change color from red to purple

---
+++ Assignment00.R
@@ -0,0 +1,14 @@
+str(mtcars)
+names(mtcars)
+head(mtcars)
+summary(mtcars)
+
+library(ggplot2)
+ggplot(mtcars, aes(mpg)) +
+  geom_histogram(binwidth = 4) + xlab('Miles per Gallon') + ylab('Number of Cars') + 
+  ggtitle('Distribution of Cars by Mileage')
+
+ggplot(mtcars,aes(x = mpg, y = hp))+
+  
+  geom_point(color = "red", size = 2.5, shape = 1)+ theme_classic()
+
