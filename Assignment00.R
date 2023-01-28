cd7c29a54eb2ff88cfeb700e4aff6390c1d7b5e1
MahshidArastonejad <arastonejadm@vcu.edu>
2023-01-27 19:04:26 -0500

    Colour the Data-points

--- Assignment00.R
+++ Assignment00.R
@@ -8,7 +8,14 @@ ggplot(mtcars, aes(mpg)) +
   geom_histogram(binwidth = 4) + xlab('Miles per Gallon') + ylab('Number of Cars') + 
   ggtitle('Distribution of Cars by Mileage')
 #creat scatterplot
+#put lables on the plot 
 ggplot(mtcars,aes(x = mpg, y = hp))+
   
-  geom_point(color = "purple", size = 2.5, shape = 1)+ theme_classic()
+  geom_point(color = "purple", size = 4.5, shape = 1)+ theme_classic()+
+  labs(title = "Scatterplot - MPG Vs HP",
+       subtitle = "Scatterplot showing Negative Correlation",
+       x = "Miles Per Gallon",
+       y = "Horsepower",
+       caption = "Source: MTCARS Dataset")
+  
 
